a3b547c7df9c34d14f2685adc4738497b129803b
parmeet9891 <parmeet_coder@yahoo.com>
2018-08-25 16:41:05 +0530

    changes in file with build folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Battle from './Battle.js';
 import Home from './Home.js';
 import Popular from './Popular.js';
